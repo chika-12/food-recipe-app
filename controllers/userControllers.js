cbf8638df29d3c6fd3dfdb0a2a711ff6ba7712bf
chika-12 <markworship001@gmail.com>
2025-05-19 10:57:37 +0100

    update cache logic

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,7 +2,8 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const RecipeUser = require('../models/userModels');
 const factory = require('./factoryFunction');
-const { populate } = require('../models/recipeModels');
+//const { populate } = require('../models/recipeModels');
+const cache = require('../utils/cache');
 const Notification = require('../models/notification');
 
 exports.postUser = catchAsync(async (req, res, next) => {
@@ -21,6 +22,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 });
 
 exports.getUserProfile = catchAsync(async (req, res, next) => {
+  const cacheKey = `RecipeUser_all`;
+  const cacheData = cache.get(cacheKey);
+
+  if (cacheData) {
+    return res.status(200).json({
+      status: 'success',
+      source: 'cache',
+      data: cacheData,
+    });
+  }
   const profile = await RecipeUser.findById(req.user.id)
     .populate({
       path: 'favorite', // first populate the virtual on User
@@ -36,6 +47,8 @@ exports.getUserProfile = catchAsync(async (req, res, next) => {
   if (!profile) {
     return next(new AppError('This user does not exist', 404));
   }
+  cache.set(cacheKey, profile.toObject());
+
   res.status(200).json({
     Status: 'Success',
     profile,
